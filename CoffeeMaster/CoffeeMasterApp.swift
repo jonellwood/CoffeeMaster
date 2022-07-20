5d342a8c40cbc6638b655ce1d30d21bce4ec7c37
Jon Ellwood <jonellwood@gmail.com>
2022-07-20 08:36:23 -0400

    Initial Commit

---
+++ CoffeeMaster/CoffeeMasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeMasterApp.swift
+//  CoffeeMaster
+//
+//  Created by Jonathan Ellwood on 7/20/22.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeMasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CoffeeMaster/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  CoffeeMaster
+//
+//  Created by Jonathan Ellwood on 7/20/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
